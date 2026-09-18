5637bc3ec06792b3c634fe242e248136c0e297be
Vitaly Skadorva <vitaly@Vitalys-MacBook-M1.local>
2026-09-17 20:53:27 -0400

    deleted wrong test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,8 +16,3 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  await expect(page).toHaveTitle('Google');
-});
